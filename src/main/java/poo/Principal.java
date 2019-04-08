c4aba17688eb16357e9b4e936a53d05bc2677b7d
Faracoeng <alfmazucheli>
2019-04-08 09:29:33 -0300

    testaTempo iniciada testes iniciados

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Insira a hora: ");
-        int hora = teclado.nextInt();
-        System.out.println("Insira o minuto: ");
-        int minutos = teclado.nextInt();
-        System.out.println("Insira os segundos: ");
-        int segundos = teclado.nextInt();
-
-        Time relogio = new Time(hora, minutos, segundos );
-        Time relogio2 = new Time(minutos, hora, segundos );
-        System.out.println(relogio2.toString()+" relogio2");
-        System.out.println(relogio.toString());
-        System.out.println(relogio.tempoEmSegundos());
-        System.out.println(relogio.diferenca(relogio2));
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.println("Insira a hora: ");
+//        int hora = teclado.nextInt();
+//        System.out.println("Insira o minuto: ");
+//        int minutos = teclado.nextInt();
+//        System.out.println("Insira os segundos: ");
+//        int segundos = teclado.nextInt();
+//
+//        Time relogio = new Time(hora, minutos, segundos );
+//        Time relogio2 = new Time(minutos, hora, segundos );
+//        System.out.println(relogio2.toString()+" relogio2");
+//        System.out.println(relogio.toString());
+//        System.out.println(relogio.tempoEmSegundos());
+//        System.out.println(relogio.diferenca(relogio2));
     }
 }
--- src/test/java/testaTempo.java
+++ src/test/java/testaTempo.java
@@ -1,2 +1,16 @@
+package poo;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+
 public class testaTempo {
+    @Test
+    public void testarHorario(){
+        Time relogioTeste = new Time(60,60,60);
+        Assert.assertEquals("60:60:60 Não é um horário", "0:0:0",relogioTeste.toString());
+
+
+    }
+
 }
